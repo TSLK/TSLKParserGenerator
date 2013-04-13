535354d578ccb8198a0511154cc65253b658790f
Nick Guletskii <nickguletskii200@gmail.com>
2013-04-13 10:20:58 +0300

    Added compilation and packaging translation steps.

--- src-gen/org/ng200/tslk/lang/TSLKGrammarListener.java
+++ src-gen/org/ng200/tslk/lang/TSLKGrammarListener.java
@@ -1,81 +1,106 @@
 // Generated from TSLKGrammar.g4 by ANTLR 4.0
 package org.ng200.tslk.lang;
-import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.runtime.Token;
+
+import org.antlr.v4.runtime.tree.ParseTreeListener;
 
 public interface TSLKGrammarListener extends ParseTreeListener {
-	void enterBody(TSLKGrammarParser.BodyContext ctx);
-	void exitBody(TSLKGrammarParser.BodyContext ctx);
-
-	void enterBreakStmt(TSLKGrammarParser.BreakStmtContext ctx);
-	void exitBreakStmt(TSLKGrammarParser.BreakStmtContext ctx);
-
-	void enterModifyExpr(TSLKGrammarParser.ModifyExprContext ctx);
-	void exitModifyExpr(TSLKGrammarParser.ModifyExprContext ctx);
-
-	void enterPathCall(TSLKGrammarParser.PathCallContext ctx);
-	void exitPathCall(TSLKGrammarParser.PathCallContext ctx);
-
-	void enterAtomString(TSLKGrammarParser.AtomStringContext ctx);
-	void exitAtomString(TSLKGrammarParser.AtomStringContext ctx);
+	void enterAssignExpr(TSLKGrammarParser.AssignExprContext ctx);
 
 	void enterAtomBooleanFalse(TSLKGrammarParser.AtomBooleanFalseContext ctx);
-	void exitAtomBooleanFalse(TSLKGrammarParser.AtomBooleanFalseContext ctx);
-
-	void enterAssignExpr(TSLKGrammarParser.AssignExprContext ctx);
-	void exitAssignExpr(TSLKGrammarParser.AssignExprContext ctx);
-
-	void enterLocalAssignExpr(TSLKGrammarParser.LocalAssignExprContext ctx);
-	void exitLocalAssignExpr(TSLKGrammarParser.LocalAssignExprContext ctx);
-
-	void enterTableBlock(TSLKGrammarParser.TableBlockContext ctx);
-	void exitTableBlock(TSLKGrammarParser.TableBlockContext ctx);
-
-	void enterSubExpr(TSLKGrammarParser.SubExprContext ctx);
-	void exitSubExpr(TSLKGrammarParser.SubExprContext ctx);
-
-	void enterAtomNumber(TSLKGrammarParser.AtomNumberContext ctx);
-	void exitAtomNumber(TSLKGrammarParser.AtomNumberContext ctx);
-
-	void enterContinueStmt(TSLKGrammarParser.ContinueStmtContext ctx);
-	void exitContinueStmt(TSLKGrammarParser.ContinueStmtContext ctx);
-
-	void enterReturnStmt(TSLKGrammarParser.ReturnStmtContext ctx);
-	void exitReturnStmt(TSLKGrammarParser.ReturnStmtContext ctx);
-
-	void enterNormalStmt(TSLKGrammarParser.NormalStmtContext ctx);
-	void exitNormalStmt(TSLKGrammarParser.NormalStmtContext ctx);
-
-	void enterFuncCall(TSLKGrammarParser.FuncCallContext ctx);
-	void exitFuncCall(TSLKGrammarParser.FuncCallContext ctx);
-
-	void enterIfBlock(TSLKGrammarParser.IfBlockContext ctx);
-	void exitIfBlock(TSLKGrammarParser.IfBlockContext ctx);
-
-	void enterFuncBlock(TSLKGrammarParser.FuncBlockContext ctx);
-	void exitFuncBlock(TSLKGrammarParser.FuncBlockContext ctx);
-
-	void enterBinaryOperator(TSLKGrammarParser.BinaryOperatorContext ctx);
-	void exitBinaryOperator(TSLKGrammarParser.BinaryOperatorContext ctx);
-
-	void enterWhileBlock(TSLKGrammarParser.WhileBlockContext ctx);
-	void exitWhileBlock(TSLKGrammarParser.WhileBlockContext ctx);
-
-	void enterUnaryOperator(TSLKGrammarParser.UnaryOperatorContext ctx);
-	void exitUnaryOperator(TSLKGrammarParser.UnaryOperatorContext ctx);
-
-	void enterTablenode(TSLKGrammarParser.TablenodeContext ctx);
-	void exitTablenode(TSLKGrammarParser.TablenodeContext ctx);
-
-	void enterStaticChildCall(TSLKGrammarParser.StaticChildCallContext ctx);
-	void exitStaticChildCall(TSLKGrammarParser.StaticChildCallContext ctx);
-
-	void enterForBlock(TSLKGrammarParser.ForBlockContext ctx);
-	void exitForBlock(TSLKGrammarParser.ForBlockContext ctx);
-
-	void enterDynamicChildCall(TSLKGrammarParser.DynamicChildCallContext ctx);
-	void exitDynamicChildCall(TSLKGrammarParser.DynamicChildCallContext ctx);
 
 	void enterAtomBooleanTrue(TSLKGrammarParser.AtomBooleanTrueContext ctx);
+
+	void enterAtomNumber(TSLKGrammarParser.AtomNumberContext ctx);
+
+	void enterAtomString(TSLKGrammarParser.AtomStringContext ctx);
+
+	void enterBinaryOperator(TSLKGrammarParser.BinaryOperatorContext ctx);
+
+	void enterBody(TSLKGrammarParser.BodyContext ctx);
+
+	void enterBreakStmt(TSLKGrammarParser.BreakStmtContext ctx);
+
+	void enterContinueStmt(TSLKGrammarParser.ContinueStmtContext ctx);
+
+	void enterDynamicChildCall(TSLKGrammarParser.DynamicChildCallContext ctx);
+
+	void enterForBlock(TSLKGrammarParser.ForBlockContext ctx);
+
+	void enterFuncBlock(TSLKGrammarParser.FuncBlockContext ctx);
+
+	void enterFuncCall(TSLKGrammarParser.FuncCallContext ctx);
+
+	void enterIfBlock(TSLKGrammarParser.IfBlockContext ctx);
+
+	void enterLocalAssignExpr(TSLKGrammarParser.LocalAssignExprContext ctx);
+
+	void enterModifyExpr(TSLKGrammarParser.ModifyExprContext ctx);
+
+	void enterNormalStmt(TSLKGrammarParser.NormalStmtContext ctx);
+
+	void enterPathCall(TSLKGrammarParser.PathCallContext ctx);
+
+	void enterReturnStmt(TSLKGrammarParser.ReturnStmtContext ctx);
+
+	void enterStaticChildCall(TSLKGrammarParser.StaticChildCallContext ctx);
+
+	void enterSubExpr(TSLKGrammarParser.SubExprContext ctx);
+
+	void enterTableBlock(TSLKGrammarParser.TableBlockContext ctx);
+
+	void enterTablenode(TSLKGrammarParser.TablenodeContext ctx);
+
+	void enterUnaryOperator(TSLKGrammarParser.UnaryOperatorContext ctx);
+
+	void enterWhileBlock(TSLKGrammarParser.WhileBlockContext ctx);
+
+	void exitAssignExpr(TSLKGrammarParser.AssignExprContext ctx);
+
+	void exitAtomBooleanFalse(TSLKGrammarParser.AtomBooleanFalseContext ctx);
+
 	void exitAtomBooleanTrue(TSLKGrammarParser.AtomBooleanTrueContext ctx);
+
+	void exitAtomNumber(TSLKGrammarParser.AtomNumberContext ctx);
+
+	void exitAtomString(TSLKGrammarParser.AtomStringContext ctx);
+
+	void exitBinaryOperator(TSLKGrammarParser.BinaryOperatorContext ctx);
+
+	void exitBody(TSLKGrammarParser.BodyContext ctx);
+
+	void exitBreakStmt(TSLKGrammarParser.BreakStmtContext ctx);
+
+	void exitContinueStmt(TSLKGrammarParser.ContinueStmtContext ctx);
+
+	void exitDynamicChildCall(TSLKGrammarParser.DynamicChildCallContext ctx);
+
+	void exitForBlock(TSLKGrammarParser.ForBlockContext ctx);
+
+	void exitFuncBlock(TSLKGrammarParser.FuncBlockContext ctx);
+
+	void exitFuncCall(TSLKGrammarParser.FuncCallContext ctx);
+
+	void exitIfBlock(TSLKGrammarParser.IfBlockContext ctx);
+
+	void exitLocalAssignExpr(TSLKGrammarParser.LocalAssignExprContext ctx);
+
+	void exitModifyExpr(TSLKGrammarParser.ModifyExprContext ctx);
+
+	void exitNormalStmt(TSLKGrammarParser.NormalStmtContext ctx);
+
+	void exitPathCall(TSLKGrammarParser.PathCallContext ctx);
+
+	void exitReturnStmt(TSLKGrammarParser.ReturnStmtContext ctx);
+
+	void exitStaticChildCall(TSLKGrammarParser.StaticChildCallContext ctx);
+
+	void exitSubExpr(TSLKGrammarParser.SubExprContext ctx);
+
+	void exitTableBlock(TSLKGrammarParser.TableBlockContext ctx);
+
+	void exitTablenode(TSLKGrammarParser.TablenodeContext ctx);
+
+	void exitUnaryOperator(TSLKGrammarParser.UnaryOperatorContext ctx);
+
+	void exitWhileBlock(TSLKGrammarParser.WhileBlockContext ctx);
 }
--- src/org/ng200/tslk/langgen/TSLKGenerator.java
+++ src/org/ng200/tslk/langgen/TSLKGenerator.java
@@ -10,14 +10,30 @@
  ******************************************************************************/
 package org.ng200.tslk.langgen;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
 import org.antlr.v4.Tool;
 import org.stringtemplate.v4.ST;
@@ -26,7 +42,103 @@ import org.stringtemplate.v4.misc.STMessage;
 
 public class TSLKGenerator {
 
-	public TSLKGenerator() {
+	public static void addToZip(File file, ZipOutputStream zipOutputStream)
+			throws IOException {
+		FileInputStream fileInput = new FileInputStream(file);
+		ZipEntry zipEntry = new ZipEntry(file.getPath().replaceFirst(
+				"src-gen/", ""));
+		zipOutputStream.putNextEntry(zipEntry);
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fileInput.read(bytes)) >= 0) {
+			zipOutputStream.write(bytes, 0, length);
+		}
+		zipOutputStream.closeEntry();
+		fileInput.close();
+	}
+
+	public static void cleanup() {
+		new File("TSLKGrammar.g4").delete();
+	}
+
+	private static void compile() throws IOException {
+		final ArrayList<File> files = new ArrayList<File>();
+		Path startPath = Paths.get("src-gen");
+		Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				System.out.println(file);
+				if (file.getFileName().toString().endsWith(".java"))
+					files.add(file.toFile());
+				return FileVisitResult.CONTINUE;
+			}
+		});
+		System.out.println(files);
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		StandardJavaFileManager fileManager = compiler.getStandardFileManager(
+				null, null, null);
+
+		Iterable<? extends JavaFileObject> compilationUnits1 = fileManager
+				.getJavaFileObjectsFromFiles(files);
+		compiler.getTask(null, fileManager, null, null, null, compilationUnits1)
+				.call();
+
+		fileManager.close();
+	}
+
+	public static void createJar() throws IOException {
+		FileOutputStream f = new FileOutputStream("TSLKTranslation.jar");
+		final ZipOutputStream zip = new ZipOutputStream(
+				new BufferedOutputStream(f));
+		Path startPath = Paths.get("src-gen");
+		Files.walkFileTree(startPath, new SimpleFileVisitor<Path>() {
+
+			@Override
+			public FileVisitResult visitFile(Path file,
+					BasicFileAttributes attrs) throws IOException {
+				addToZip(file.toFile(), zip);
+				return FileVisitResult.CONTINUE;
+			}
+
+		});
+		zip.close();
+	}
+
+	public static void generateGrammar(String templateString,
+			HashMap<String, String> modifiableFields) throws IOException {
+		ST template = new ST(templateString);
+		for (String s : modifiableFields.keySet())
+			template.add(s, modifiableFields.get(s));
+
+		template.write(new File("TSLKGrammar.g4"), new STErrorListener() {
+			@Override
+			public void compileTimeError(STMessage arg0) {
+				System.err.println(arg0);
+			}
+
+			@Override
+			public void internalError(STMessage arg0) {
+				System.err.println(arg0);
+
+			}
+
+			@Override
+			public void IOError(STMessage arg0) {
+				System.err.println(arg0);
+			}
+
+			@Override
+			public void runTimeError(STMessage arg0) {
+				System.err.println(arg0);
+			}
+		});
+	}
+
+	public static void generateJavaSourcecode() {
+		String[] toolArgs = { "-o", "src-gen/org/ng200/tslk/lang", "-package",
+				"org.ng200.tslk.lang", "TSLKGrammar.g4" };
+		Tool.main(toolArgs);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -50,45 +162,11 @@ public class TSLKGenerator {
 					applicationProps.getProperty(key.toString()));
 		generateGrammar(templateString, modifiableFields);
 		generateJavaSourcecode();
+		compile();
+		createJar();
+		cleanup();
 	}
 
-	public static void generateGrammar(String templateString,
-			HashMap<String, String> modifiableFields) throws IOException {
-		ST template = new ST(templateString);
-		for (String s : modifiableFields.keySet())
-			template.add(s, modifiableFields.get(s));
-
-		template.write(new File("TSLKGrammar.g4"), new STErrorListener() {
-			@Override
-			public void runTimeError(STMessage arg0) {
-				System.err.println(arg0);
-			}
-
-			@Override
-			public void internalError(STMessage arg0) {
-				System.err.println(arg0);
-
-			}
-
-			@Override
-			public void compileTimeError(STMessage arg0) {
-				System.err.println(arg0);
-			}
-
-			@Override
-			public void IOError(STMessage arg0) {
-				System.err.println(arg0);
-			}
-		});
-	}
-
-	public static void generateJavaSourcecode() {
-		String[] toolArgs = { "-o", "src-gen/org/ng200/tslk/lang", "-package",
-				"org.ng200.tslk.lang", "TSLKGrammar.g4" };
-		Tool.main(toolArgs);
-	}
-
-	public static void cleanup() {
-		new File("TSLKGrammar.g4").delete();
+	public TSLKGenerator() {
 	}
 }
